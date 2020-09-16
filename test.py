dad027c6f42beb766f1e1ce1153327754acc9d4a
chienbk98 <yoonachien@gmail.com>
2020-09-16 11:13:37 +0700

    sua lai file test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from myUI import Ui_MainWindow
+from myUI import Ui_MainWindow, Ui_Form
 import myUI
 from PyQt5 import QtWidgets
 import sys
@@ -32,34 +32,19 @@ def creatingPipeline():
       print("Exiting app\n")
       pipeline.set_state(Gst.State.NULL)
 if __name__ == "__main__":
-    # a = cv2.imread("/home/yoona/Desktop/learn_python/PyQt5/home.jpg")
-    # b = cv2.imread("/home/yoona/Pictures/84534587_2692473420836014_4507958916096720896_o.jpg")
-    # c = cv2.imread("/home/yoona/Pictures/81041837_2692472010836155_3651594724206182400_o.jpg")
-    # myUI.image_result[0] = a
-    # myUI.image_result[1] = b
-    # myUI.image_result[2] = c
-    
-    # t1 = threading.Thread(target=capture_image, args=(0, 0, ))
-    # t1.setDaemon(True)
-    # t2 = threading.Thread(target=capture_image, args=(1, "rtsp://192.168.1.9:43794", ))
-    # t2.setDaemon(True)
-    # t3 = threading.Thread(target=capture_image, args=(2,"rtsp://192.168.1.9:43794", ))
-    # t3.setDaemon(True)
+    # App = QtWidgets.QApplication(sys.argv)
+    # mainWindow = QtWidgets.QWidget()
+    # ui = Ui_MainWindow()
+    # ui.setupUi(mainWindow)
+    # mainWindow.show()
 
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    # sys.exit(app.exec_())
 
-    # t1.start()
-    # t2.start()
-    # t3.start()
-
-
-    # t = threading.Thread(target=creatingPipeline, args=())
-    # t.setDaemon(True)
-    # t.start()
-    App = QtWidgets.QApplication(sys.argv)
-    mainWindow = QtWidgets.QWidget()
-    ui = Ui_MainWindow()
-    ui.setupUi(mainWindow)
-    mainWindow.show()
 
     pp = PipeLine(on_screen_display=False, tracking=True)
     pipeline = pp.pipeline
